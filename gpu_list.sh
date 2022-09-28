14b2c7df5ab2e8a9a7d14e48ae76a26fae4ba6ff
bboysloth <jeremyhannan@gmail.com>
2022-09-28 12:24:47 -0600

    Create gpu_list.sh

---
+++ gpu_list.sh
@@ -0,0 +1,6 @@
+# simple gcloud commands for extracting all instances with a GPU attached
+# outputs to the console as a table
+
+#!/bin/bash
+
+for proj in $(gcloud alpha projects list --folder=189084715210 --format='value(PROJECT_ID)'); do for gpu_zone in $(gcloud compute instances list --project=$proj --format='value(ZONE)'); do for gpu_attached in $(gcloud compute instances list --project=$proj --zones=$gpu_zone --format='value(NAME)'); do gcloud compute instances describe $gpu_attached --project=$proj --zone=$gpu_zone --format="table(name,zone.basename(),guestAccelerators[].acceleratorCount,guestAccelerators[].acceleratorType.basename())"; done; done; done
